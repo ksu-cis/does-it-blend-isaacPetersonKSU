3a541b8acfa2a2f79ecbdbc014062a96807124e0
ijp <ijp@CIS-RDS4>
2020-02-05 15:37:00 -0600

    test #1

--- DoesItBlend/Program.cs
+++ DoesItBlend/Program.cs
@@ -7,6 +7,14 @@ namespace DoesItBlend
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var b = new Banana();
+            var m = new Mango();
+            var s = new Strawberry();
+
+            Console.Write(b.Blend());
+            Console.Write(m.Blend());
+            Console.Write(s.Blend());
         }
     }
 }
